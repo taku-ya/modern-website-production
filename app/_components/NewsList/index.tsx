da337b74bf7501d444dc2fec54d6c6fdc71c4683
{taku-ya} <{s.platinum.0529@gmail.com}>
2024-07-30 18:55:59 +0900

    4章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -4,6 +4,7 @@ import styles from "./index.module.css";
 import type { News } from "@/app/_libs/microcms";
 import Category from "../Category";
 import Date from "../Date";
+import Link from "next/link";
 
 type Props = {
   news: News[];
@@ -17,8 +18,7 @@ export default function NewsList({ news }: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          {/* TODO:別の章で、リンクと正式な画像に差し替えます */}
-          <div className={styles.link}>
+          <Link href={`/news/${article.id}`} className={styles.link}>
             <Image
               className={styles.image}
               src="/no-image.png"
@@ -33,7 +33,7 @@ export default function NewsList({ news }: Props) {
                 <Date date={article.publishedAt ?? article.createdAt} />
               </dd>
             </dl>
-          </div>
+          </Link>
         </li>
       ))}
     </ul>
