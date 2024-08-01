0a3e8226ded1aa4de7131f27d34f0397892dcd0c
{taku-ya} <{s.platinum.0529@gmail.com}>
2024-08-01 17:55:54 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import { getNewsList } from "../_libs/microcms";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
