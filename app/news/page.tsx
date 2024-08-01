ef54634c68326487c25d6e2c6d2feb0b5f7d0d7d
{taku-ya} <{s.platinum.0529@gmail.com}>
2024-08-01 17:59:46 +0900

    ニュースにISRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,7 +4,7 @@ import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import { getNewsList } from "../_libs/microcms";
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
