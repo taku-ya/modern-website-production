dfe57a745a88caea9bb83c73131fba6cb07cc2e7
{taku-ya} <{s.platinum.0529@gmail.com}>
2024-08-02 15:37:57 +0900

    10章まで完了

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,27 @@
+import { Metadata } from "next";
 import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import "./globals.css";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://modern-website-production.vercel.app/"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description:
+    "「Next.js＋ヘッドレスCMSではじめる！ かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description:
+      "「Next.js＋ヘッドレスCMSではじめる！ かんたん・モダンWebサイト制作入門」で作成されるサイトです。",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "https://modern-website-production.vercel.app/",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -14,6 +35,8 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      {/* <GoogleAnalytics gaId="G-S9DDMJLENH" /> */}
+      <GoogleTagManager gtmId="GTM-5F4QFSR8" />
     </html>
   );
 }
--- app/members/layout.tsx
+++ app/members/layout.tsx
@@ -1,6 +1,10 @@
 import Hero from "../_components/Hero";
 import Sheet from "../_components/Sheet";
 
+export const metadata = {
+  title: "メンバー",
+};
+
 type Props = {
   children: React.ReactNode;
 };
